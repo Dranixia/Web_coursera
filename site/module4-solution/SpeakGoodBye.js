0d05d291de3a817dda93609e745cf5ff79727a88
Dranixia <dranixoverlord@gmail.com>
2020-04-17 03:56:45 +0300

    4th task

---
+++ site/module4-solution/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function (window) {
+	var byeSpeak = {};
+
+	var speakWord = "Good Bye";
+
+	byeSpeak.speak = function (name) {
+		console.log(speakWord + " " + name)
+	} 
+	
+	window.byeSpeak = byeSpeak;
+})(window)
